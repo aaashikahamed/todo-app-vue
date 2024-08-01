c7c7992002a25c3a70b7e21a02c701d4e19ca4fa
aaashikahamed <ahamedaashik071@gmail.com>
2024-08-01 11:34:39 +0530

    Project Initialization

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  // these things we can use with composition API
+  //  ref - How we are gonna handle the states
+  // onMounted - going to be used when we render the page or component. this will used to call our storage out / use storage
+  // 
+  import {ref, onMounted, computed, watch} from 'vue'
+</script>
+
+<template>
+  <div>
+    hello world
+  </div>
+</template>
