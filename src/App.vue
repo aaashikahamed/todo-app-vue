ee955f1e0ccfacd7b1bf711a19c96b181ea09434
aaashikahamed <ahamedaashik071@gmail.com>
2024-08-01 12:34:09 +0530

    completed and some functions will be added in the future

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,14 @@
       done: false,
       createdAt: new Date().getTime()
     })
+
+    input_content.value = ''
+    input_category.value = null
+
+  }
+
+  const removeTodo = todo => {
+    todos.value = todos.value.filter(t => t !== todo)
   }
 
   // this will only work when we set our 'todos' for first time
@@ -84,6 +92,25 @@
         <input type="submit" value="Add todo">
 
       </form>
+     </section>
+
+     <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+          <label >
+            <input type="checkbox" name=""  v-model="todo.done" id="">
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
      </section>
   </main>
 
